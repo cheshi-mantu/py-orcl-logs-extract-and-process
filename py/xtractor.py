c58384ec8abebf5104b33da0926271cb8d2d010e
cheshi-mantu <cheshi.mantu@gmail.com>
2020-06-03 14:33:06 +0300

    refactoring combined all extraction operations in one procedure

--- py/xtractor.py
+++ py/xtractor.py
@@ -52,48 +52,47 @@ def debugMove(strFilesFolder):
 
 # extract files from .Z archives, move Z archives and archives with "debug" string in file name to done folder
 # we have no use for debug files so no further extraction for them is needed
-def extractZandMove(str7ZipSysExecPath, strFilesFolder):
-    strCmdLineZ = " x -o" + strFilesFolder + " "
+def extractAndMove(str7ZipSysExecPath, strFilesFolder, listStrToExtract, strHowToExtract):
+    if strHowToExtract == "here":
+        strCmdLineZ = " x -o" + strFilesFolder + " "
+    elif strHowToExtract == "subfolder":
+        strCmdLineZ = " x -o" + strFilesFolder + "\\* "
     # traversing the strFileFolders for all files that contain "Collection" in their names
     #first pass is to extract all .Z files as they are archives with tar inside and we need tar to be extracted
     for root, dirs, files in os.walk(strFilesFolder):
         for file in files:
-            if (("debug_" not in file) and ("tar.Z" in file)) or (("debug_" not in file) and ("tar.bz2" in file)):
-                print(f"Processing {root}\\{file}")
-                os.system(str7ZipSysExecPath + strCmdLineZ + root + "\\" + file)
-    # move all .Z files and Debug archives to done folder
-    # Debug files have no practival use at the moment
-    for root, dirs, files in os.walk(strFilesFolder):
-        if not os.path.exists(strFilesFolder+"\\done"):
-            os.mkdir(strFilesFolder+"\\done")
-        for file in files:
-            if ("debug_" in file) or ("tar.Z" in file) or ("tar.bz2" in file):
-                os.replace(root+"\\"+file, root+"\\done\\"+file)
+            # if ("tar.Z" in file) or ("tar.bz2" in file):
+            #     print(f"Processing {root}\\{file}")
+            #     os.system(str7ZipSysExecPath + strCmdLineZ + root + "\\" + file)
+            #     os.replace(root + "\\" + file, root + "\\done\\" + file)
+            for testSubstring in listStrToExtract:
+                if testSubstring in file:
+                    print(f"Processing {root}\\{file}")
+                    os.system(str7ZipSysExecPath + strCmdLineZ + root + "\\" + file)
+                    os.replace(root + "\\" + file, root + "\\done\\" + file)
         break
 #
-def extractTarsAndMove(str7ZipSysExecPath, strFilesFolder):
-    #command line for 7Zip executable for tar archives extraction
-    strCmdLine = " x -o" + strFilesFolder + "\\* "
-    #second pass: extracting data from tar files
-    # #traverse 1st level of the os.walk
-    for root, dirs, files in os.walk(strFilesFolder):
-        if "done" not in root:
-            for file in files:
-                if "Collection" in file:
-                    print(f"Processing {root}\\{file}")
-                    os.system(str7ZipSysExecPath + strCmdLine + root + "\\" + file)
-                    try:
-                        os.replace(root + "\\" + file, root + "\\done\\" + file)
-                    except:
-                        print("Something is troubling me")
-            break
+# def extractTarsAndMove(str7ZipSysExecPath, strFilesFolder):
+#     #command line for 7Zip executable for tar archives extraction
+#
+#     #second pass: extracting data from tar files
+#     # #traverse 1st level of the os.walk
+#     for root, dirs, files in os.walk(strFilesFolder):
+#         if "done" not in root:
+#             for file in files:
+#                 if ".tar" in file:
+#                     print(f"Processing {root}\\{file}")
+#                     os.system(str7ZipSysExecPath + strCmdLine + root + "\\" + file)
+#                     try:
+#                         os.replace(root + "\\" + file, root + "\\done\\" + file)
+#                     except:
+#                         print("Something is troubling me")
+#             break
 """
 Now runnig the scripts
 """
 if extractorSysCheck():
     strFilesFolder = getWorkingFolder(strFilesFolder)
     debugMove(strFilesFolder)
-    extractZandMove(str7ZipSysExecPath, strFilesFolder)
-    extractTarsAndMove(str7ZipSysExecPath, strFilesFolder)
-
-
+    extractAndMove(str7ZipSysExecPath, strFilesFolder, [".tar.Z", ".tar.bz"], "here")
+    extractAndMove(str7ZipSysExecPath, strFilesFolder, [".tar"], "subfolder")
